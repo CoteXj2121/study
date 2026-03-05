6d9e6d97142b77da04d644643bd74272bb405b33
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:44 +0500

    Добавить задачу 10 (Наследование)

---
+++ Наследование/task10_triangle.php
@@ -0,0 +1,31 @@
+﻿<?php
+
+class Triangle {
+    protected $side1;
+    protected $side2;
+    protected $side3;
+
+    public function __construct($side1, $side2, $side3) {
+        $this->side1 = $side1;
+        $this->side2 = $side2;
+        $this->side3 = $side3;
+    }
+
+    public function perimeter() {
+        return $this->side1 + $this->side2 + $this->side3;
+    }
+}
+
+class EquilateralTriangle extends Triangle {
+    public function __construct($side) {
+        parent::__construct($side, $side, $side);
+    }
+}
+
+// Test case
+$triangle = new Triangle(3, 4, 5);
+echo $triangle->perimeter() . "\n"; // 12
+
+$equilateral = new EquilateralTriangle(5);
+echo $equilateral->perimeter() . "\n"; // 15
+?>
