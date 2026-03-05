40ae33695d82ae4b53a0190b3c5ac14db83de2a4
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:26 +0500

    Добавить задачу 4 (Введение в ООП)

---
+++ Введение в ООП/task4_oddeven.php
@@ -0,0 +1,37 @@
+﻿<?php
+
+class OddEvenSeparator
+{
+    private $even_numbers = [];
+    private $odd_numbers = [];
+
+    public function add_number($num)
+    {
+        if ($num % 2 === 0) {
+            $this->even_numbers[] = $num;
+        } else {
+            $this->odd_numbers[] = $num;
+        }
+    }
+
+    public function even()
+    {
+        return $this->even_numbers;
+    }
+
+    public function odd()
+    {
+        return $this->odd_numbers;
+    }
+}
+
+// Test cases
+$separator = new OddEvenSeparator();
+$separator->add_number(1);
+$separator->add_number(5);
+$separator->add_number(6);
+$separator->add_number(8);
+$separator->add_number(3);
+echo implode(" ", $separator->even()) . "\n"; // 6 8
+echo implode(" ", $separator->odd()) . "\n";  // 1 5 3
+?>
