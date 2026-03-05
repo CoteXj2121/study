582ec007b5bfd4d50e48f617fcba1c5a61f5e097
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:35 +0500

    Добавить задачу 1 (Полиморфизм)

---
+++ Полиморфизм/task1_selector.php
@@ -0,0 +1,42 @@
+﻿<?php
+
+class Selector
+{
+    private $values = [];
+
+    public function __construct($values = [])
+    {
+        $this->values = $values;
+    }
+
+    public function get_odds()
+    {
+        $odds = [];
+        foreach ($this->values as $num) {
+            if ($num % 2 !== 0) {
+                $odds[] = $num;
+            }
+        }
+        return $odds;
+    }
+
+    public function get_evens()
+    {
+        $evens = [];
+        foreach ($this->values as $num) {
+            if ($num % 2 === 0) {
+                $evens[] = $num;
+            }
+        }
+        return $evens;
+    }
+}
+
+// Test case
+$values = [11, 12, 13, 14, 15, 16, 22, 44, 66];
+$selector = new Selector($values);
+$odds = $selector->get_odds();
+$evens = $selector->get_evens();
+echo implode(" ", $odds) . "\n";
+echo implode(" ", $evens) . "\n";
+?>
