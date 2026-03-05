93083c3bec5f8d77cc15d0b29dead490b5e73a63
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:27 +0500

    Добавить задачу 5 (Введение в ООП)

---
+++ Введение в ООП/task5_bigbell.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+class BigBell
+{
+    private $count = 0;
+
+    public function sound()
+    {
+        if ($this->count % 2 === 0) {
+            echo "ding\n";
+        } else {
+            echo "dong\n";
+        }
+        $this->count++;
+    }
+}
+
+// Test cases
+$bell = new BigBell();
+$bell->sound();
+$bell->sound();
+$bell->sound();
+?>
