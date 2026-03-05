7bf68c2bc3e90dfc8c9a588b4457b0583ca2201f
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:43 +0500

    Добавить задачу 5 (Наследование)

---
+++ Наследование/task5_animals.php
@@ -0,0 +1,32 @@
+﻿<?php
+
+class Animal {
+    public function breathe() {
+    }
+
+    public function eat() {
+    }
+}
+
+class Fish extends Animal {
+    public function swim() {
+    }
+}
+
+class Bird extends Animal {
+    public function lay_eggs() {
+    }
+}
+
+class FlyingBird extends Bird {
+    public function fly() {
+    }
+}
+
+// Test case
+$fish = new Fish();
+$bird = new Bird();
+$flying_bird = new FlyingBird();
+
+echo "Animal hierarchy created successfully\n";
+?>
