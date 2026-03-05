e984897c7683696f2d4930f2279663922cd54bd8
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:26 +0500

    Добавить задачу 2 (Введение в ООП)

---
+++ Введение в ООП/task2_button.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+class Button
+{
+    private $clicks = 0;
+
+    public function click()
+    {
+        $this->clicks++;
+    }
+
+    public function click_count()
+    {
+        return $this->clicks;
+    }
+
+    public function reset()
+    {
+        $this->clicks = 0;
+    }
+}
+
+// Test cases
+$button = new Button();
+$button->click();
+$button->click();
+echo $button->click_count() . "\n"; // 2
+$button->click();
+echo $button->click_count() . "\n"; // 3
+?>
