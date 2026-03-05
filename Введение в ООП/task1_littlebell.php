8c93af383d573e5d100a3e86405c49db746867e1
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:26 +0500

    Добавить задачу 1 (Введение в ООП)

---
+++ Введение в ООП/task1_littlebell.php
@@ -0,0 +1,16 @@
+﻿<?php
+
+class LittleBell
+{
+    public function sound()
+    {
+        echo "ding\n";
+    }
+}
+
+// Test cases
+$bell = new LittleBell();
+$bell->sound();
+$bell->sound();
+$bell->sound();
+?>
