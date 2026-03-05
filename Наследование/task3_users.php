546af2d2bb7e66b8006d2636ff545611002ecf76
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:43 +0500

    Добавить задачу 3 (Наследование)

---
+++ Наследование/task3_users.php
@@ -0,0 +1,33 @@
+﻿<?php
+
+class User {
+    public function solve($n) {
+    }
+}
+
+class Student extends User {
+}
+
+class Teacher extends User {
+    public function check_solution($user, $n) {
+    }
+}
+
+class Admin extends User {
+    public function edit($n) {
+    }
+}
+
+class SuperAdmin extends Admin {
+    public function grant($user) {
+    }
+}
+
+// Test case
+$student = new Student();
+$teacher = new Teacher();
+$admin = new Admin();
+$superadmin = new SuperAdmin();
+
+echo "Hierarchy created successfully\n";
+?>
