a949790d41955c68ca2073179f6b5300b0ead4e2
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:35 +0500

    Добавить задачу 4 (Полиморфизм)

---
+++ Полиморфизм/task4_stats.php
@@ -0,0 +1,70 @@
+﻿<?php
+
+class MinStat
+{
+    private $numbers = [];
+
+    public function add_number($num)
+    {
+        $this->numbers[] = $num;
+    }
+
+    public function result()
+    {
+        if (empty($this->numbers)) {
+            return null;
+        }
+        return min($this->numbers);
+    }
+}
+
+class MaxStat
+{
+    private $numbers = [];
+
+    public function add_number($num)
+    {
+        $this->numbers[] = $num;
+    }
+
+    public function result()
+    {
+        if (empty($this->numbers)) {
+            return null;
+        }
+        return max($this->numbers);
+    }
+}
+
+class AverageStat
+{
+    private $numbers = [];
+
+    public function add_number($num)
+    {
+        $this->numbers[] = $num;
+    }
+
+    public function result()
+    {
+        if (empty($this->numbers)) {
+            return null;
+        }
+        return array_sum($this->numbers) / count($this->numbers);
+    }
+}
+
+// Test case
+$values = [1, 2, 4, 5];
+
+$mins = new MinStat();
+$maxs = new MaxStat();
+$average = new AverageStat();
+foreach ($values as $v) {
+    $mins->add_number($v);
+    $maxs->add_number($v);
+    $average->add_number($v);
+}
+
+echo $mins->result() . " " . $maxs->result() . " " . sprintf("%.3f", $average->result()) . "\n";
+?>
