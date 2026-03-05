863d0ddc2c8238b8f09fefe45e68205ceaba9f12
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:43 +0500

    Добавить задачу 1 (Наследование)

---
+++ Наследование/task1_minecraft.php
@@ -0,0 +1,47 @@
+﻿<?php
+
+class BaseObject {
+    protected $x;
+    protected $y;
+    protected $z;
+
+    public function __construct($x, $y, $z) {
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+    }
+
+    public function get_coordinates() {
+        return [$this->x, $this->y, $this->z];
+    }
+}
+
+class Block extends BaseObject {
+    public function shatter() {
+        $this->x = null;
+        $this->y = null;
+        $this->z = null;
+    }
+}
+
+class Entity extends BaseObject {
+    public function move($x, $y, $z) {
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+    }
+}
+
+class Thing extends BaseObject {
+}
+
+// Test case
+$block = new Block(10, 20, 30);
+echo implode(", ", $block->get_coordinates()) . "\n";
+$block->shatter();
+echo implode(", ", array_map(function($v) { return $v === null ? "null" : $v; }, $block->get_coordinates())) . "\n";
+
+$entity = new Entity(0, 0, 0);
+$entity->move(5, 5, 5);
+echo implode(", ", $entity->get_coordinates()) . "\n";
+?>
