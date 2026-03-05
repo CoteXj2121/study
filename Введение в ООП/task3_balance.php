726916ca675b6741a4b34b79a55a0a2c76274d55
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:26 +0500

    Добавить задачу 3 (Введение в ООП)

---
+++ Введение в ООП/task3_balance.php
@@ -0,0 +1,36 @@
+﻿<?php
+
+class Balance
+{
+    private $left = 0;
+    private $right = 0;
+
+    public function add_left($weight)
+    {
+        $this->left += $weight;
+    }
+
+    public function add_right($weight)
+    {
+        $this->right += $weight;
+    }
+
+    public function result()
+    {
+        if ($this->left === $this->right) {
+            return "=";
+        } elseif ($this->left > $this->right) {
+            return "L";
+        } else {
+            return "R";
+        }
+    }
+}
+
+// Test cases
+$balance = new Balance();
+$balance->add_right(10);
+$balance->add_left(9);
+$balance->add_left(2);
+echo $balance->result() . "\n"; // L
+?>
