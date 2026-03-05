6e88c6f1a9f6a5046e5c34235accb51d16c0651c
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:43 +0500

    Добавить задачу 7 (Наследование)

---
+++ Наследование/task7_geometry.php
@@ -0,0 +1,32 @@
+﻿<?php
+
+class Shape {
+}
+
+class Polygon extends Shape {
+}
+
+class Triangle extends Polygon {
+}
+
+class IsoscelesTriangle extends Triangle {
+}
+
+class EquilateralTriangle extends IsoscelesTriangle {
+}
+
+class Quadrilateral extends Polygon {
+}
+
+class Parallelogram extends Quadrilateral {
+}
+
+class Rectangle extends Parallelogram {
+}
+
+class Square extends Rectangle {
+}
+
+// Test case
+echo "Geometry hierarchy created successfully\n";
+?>
