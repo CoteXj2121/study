cfb7e23bd1745ba5b41dd02e4c9ec8f37d41ded4
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:35 +0500

    Добавить задачу 5 (Полиморфизм)

---
+++ Полиморфизм/task5_table.php
@@ -0,0 +1,57 @@
+﻿<?php
+
+class Table
+{
+    private $rows;
+    private $cols;
+    private $data = [];
+
+    public function __construct($rows, $cols)
+    {
+        $this->rows = $rows;
+        $this->cols = $cols;
+        for ($i = 0; $i < $rows; $i++) {
+            for ($j = 0; $j < $cols; $j++) {
+                $this->data[$i][$j] = 0;
+            }
+        }
+    }
+
+    public function get_value($row, $col)
+    {
+        if ($row < 0 || $row >= $this->rows || $col < 0 || $col >= $this->cols) {
+            return null;
+        }
+        return $this->data[$row][$col];
+    }
+
+    public function set_value($row, $col, $value)
+    {
+        if ($row >= 0 && $row < $this->rows && $col >= 0 && $col < $this->cols) {
+            $this->data[$row][$col] = $value;
+        }
+    }
+
+    public function n_rows()
+    {
+        return $this->rows;
+    }
+
+    public function n_cols()
+    {
+        return $this->cols;
+    }
+}
+
+// Test case
+$tab = new Table(3, 5);
+$tab->set_value(0, 1, 10);
+$tab->set_value(1, 2, 20);
+$tab->set_value(2, 3, 30);
+for ($i = 0; $i < $tab->n_rows(); $i++) {
+    for ($j = 0; $j < $tab->n_cols(); $j++) {
+        echo $tab->get_value($i, $j) . " ";
+    }
+    echo "\n";
+}
+?>
