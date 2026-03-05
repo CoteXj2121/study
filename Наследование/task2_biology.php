961eb865be3ac8f754667a1238fed0277d89fcbf
Габбасов Динис Салаватович <coteXj2121@gmail.com>
2026-03-05 17:51:43 +0500

    Добавить задачу 2 (Наследование)

---
+++ Наследование/task2_biology.php
@@ -0,0 +1,36 @@
+﻿<?php
+
+class Acellularia {
+}
+
+class Cellularia {
+}
+
+class Prokaryota extends Cellularia {
+}
+
+class Eukaryota extends Cellularia {
+}
+
+class Unicellularia extends Eukaryota {
+}
+
+class Fungi extends Eukaryota {
+}
+
+class Plantae extends Eukaryota {
+}
+
+class Animalia extends Eukaryota {
+}
+
+// Test case - just structure verification
+$virus = new Acellularia();
+$bacteria = new Prokaryota();
+$unicell = new Unicellularia();
+$fungus = new Fungi();
+$plant = new Plantae();
+$animal = new Animalia();
+
+echo "Hierarchy created successfully\n";
+?>
